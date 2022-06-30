a6eca0536bf7c4df6c6fd5bb218adf1f07bb7699
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-06-30 21:18:26 +0100

    tracing init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use tracing::{event, span, Level};
+use tracing_subscriber::FmtSubscriber;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -12,9 +14,19 @@ struct Args {
 }
 
 fn main() -> Result<()> {
+    let subscrber = FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscrber)
+    .expect("setting tracing default failed");
+
+    let span = span!(Level::INFO, "parsing");
+
+    let _enter = span.enter();
+
     let args = Args::parse();
     let input_file_content = std::fs::read_to_string(&args.input_file)
         .with_context(|| format!("could not read file `{}`", &args.input_file))?;
-    println!("{}", input_file_content);
+
+    event!(Level::INFO, input_file_content = input_file_content, "input file content");
+    
     Ok(())
 }
