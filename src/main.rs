7020ce857516a43aa49cc2a48430f6dc5abe3411
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-06-30 21:38:55 +0100

    walk value tree

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use serde_json::Value;
 use tracing::{event, span, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -15,8 +16,7 @@ struct Args {
 
 fn main() -> Result<()> {
     let subscrber = FmtSubscriber::new();
-    tracing::subscriber::set_global_default(subscrber)
-    .expect("setting tracing default failed");
+    tracing::subscriber::set_global_default(subscrber).expect("setting tracing default failed");
 
     let span = span!(Level::INFO, "parsing");
 
@@ -26,7 +26,47 @@ fn main() -> Result<()> {
     let input_file_content = std::fs::read_to_string(&args.input_file)
         .with_context(|| format!("could not read file `{}`", &args.input_file))?;
 
-    event!(Level::INFO, input_file_content = input_file_content, "input file content");
-    
+    event!(
+        Level::INFO,
+        input_file_content = input_file_content,
+        "input file content"
+    );
+
+    let v: Value = serde_json::from_str(input_file_content.as_str())
+        .with_context(|| format!("could not parse json"))?;
+
+    walk_value_tree(&v);
+
     Ok(())
+}
+
+#[tracing::instrument]
+fn walk_value_tree(v: &Value) {
+    match v {
+        Value::Object(o) => {
+            for (k, v) in o {
+                event!(Level::INFO, "{}: ", k);
+                walk_value_tree(v);
+            }
+        }
+        Value::Array(a) => {
+            for v in a {
+                walk_value_tree(v);
+            }
+        }
+        _ => {
+            event!(Level::INFO, "{}", classify_value_type(v));
+        }
+    }
+}
+
+#[tracing::instrument]
+fn classify_value_type(v: &Value) -> String {
+    match v {
+        Value::Bool(_) => "boolean".to_string(),
+        Value::String(_) => "string".to_string(),
+        Value::Number(_) => "number".to_string(),
+        Value::Null => "never".to_string(),
+        _ => "unknown".to_string(),
+    }
 }
