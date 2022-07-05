706e074748a9e3e816b7ae8859b8ab84fddb6618
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-07-05 22:59:51 +0100

    merkle tree & common type caching

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use itertools::Itertools;
 use serde_json::Value;
-use std::collections::HashSet;
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap, HashSet},
+    hash::Hasher,
+    sync::{Arc, Mutex},
+};
 use tracing::{event, span, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -13,6 +17,21 @@ struct Args {
 
     #[clap(short = 'o', long = "output", value_parser)]
     output_file: Option<String>,
+
+    #[clap(short = 's', long = "squash", value_parser)]
+    squash_common_types: Option<bool>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct TypeOutputCacheEntry {
+    type_name: String,
+    output: String,
+}
+
+impl TypeOutputCacheEntry {
+    fn new(type_name: String, output: String) -> Self {
+        Self { type_name, output }
+    }
 }
 
 #[derive(Debug)]
@@ -25,8 +44,22 @@ enum TypeScriptPrimativeType {
     Null,
 }
 
+impl TypeScriptPrimativeType {
+    fn as_bytes(&self) -> &'static [u8] {
+        match self {
+            Self::String => b"string",
+            Self::Boolean => b"boolean",
+            Self::Number => b"number",
+            Self::Object => b"object",
+            Self::Array => b"array",
+            Self::Null => b"null",
+        }
+    }
+}
+
 #[derive(Debug)]
 struct TypeScriptNode {
+    lookup_table: Arc<Mutex<HashMap<u64, usize>>>,
     name: Option<String>,
     nullable: bool,
     optional: bool,
@@ -34,10 +67,12 @@ struct TypeScriptNode {
     root_node: bool,
     sub_items: Vec<TypeScriptNode>,
     type_signature: TypeScriptPrimativeType,
+    hash: u64,
 }
 
 impl TypeScriptNode {
     fn new(
+        lookup_table: Arc<Mutex<HashMap<u64, usize>>>,
         type_name: TypeScriptPrimativeType,
         optional: bool,
         nullable: bool,
@@ -45,6 +80,7 @@ impl TypeScriptNode {
         root_node: bool,
     ) -> Self {
         TypeScriptNode {
+            lookup_table,
             name: None,
             nullable,
             optional,
@@ -52,7 +88,34 @@ impl TypeScriptNode {
             root_node,
             sub_items: Vec::new(),
             type_signature: type_name,
+            hash: 0,
         }
+    }
+
+    fn calculate_hash(&mut self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        hasher.write(self.type_signature.as_bytes());
+        hasher.write(self.name.as_ref().unwrap_or(&"".to_string()).as_bytes());
+        let mut hash_seen_before = HashSet::<u64>::new();
+        for sub_item in &mut self.sub_items {
+            let sub_node_hash = &sub_item.calculate_hash();
+            if hash_seen_before.contains(sub_node_hash) {
+                continue;
+            } else {
+                hasher.write(&sub_node_hash.to_le_bytes());
+                hash_seen_before.insert(*sub_node_hash);
+            }
+        }
+        let hash = hasher.finish();
+        let mut table = self.lookup_table.lock().unwrap();
+        if !table.contains_key(&hash) {
+            table.insert(hash, 1);
+        } else {
+            let current_value = table[&hash];
+            table.insert(hash, current_value + 1);
+        }
+        self.hash = hash;
+        hash
     }
 
     fn with_sub_items(mut self, sub_items: Vec<TypeScriptNode>) -> Self {
@@ -63,13 +126,6 @@ impl TypeScriptNode {
     fn with_name(mut self, name: String) -> Self {
         self.name = Some(name);
         self
-    }
-
-    fn to_type_string(node: TypeScriptNode, array_node: bool) -> String {
-        let mut type_string = String::new();
-        type_string.push_str("type DefaultType = ");
-        type_string.push_str(&Self::to_type_string_helper(node, array_node, 0));
-        type_string
     }
 
     fn newline_if_parent_not_array_node(array_node: bool) -> String {
@@ -100,10 +156,32 @@ impl TypeScriptNode {
         string.chars().all(|c| c.is_alphanumeric() || c == '_')
     }
 
+    fn to_type_string(node: TypeScriptNode, array_node: bool) -> String {
+        let mut type_output_cache = HashMap::<u64, TypeOutputCacheEntry>::new();
+        let mut type_string = String::new();
+        type_string.push_str("type DefaultType = ");
+        type_string.push_str(&Self::to_type_string_helper(
+            node,
+            array_node,
+            0,
+            &mut type_output_cache,
+        ));
+        type_output_cache
+            .into_iter()
+            .sorted()
+            .for_each(|(_, value)| {
+                type_string.push_str(
+                    format!("\ntype {} = {}\n", &value.type_name, &value.output).as_str(),
+                );
+            });
+        type_string
+    }
+
     fn to_type_string_helper(
         node: TypeScriptNode,
         parent_array_node: bool,
         indent_size: usize,
+        type_output_cache: &mut HashMap<u64, TypeOutputCacheEntry>,
     ) -> String {
         let mut type_string = String::new();
         let mut indent_string = String::new();
@@ -129,36 +207,60 @@ impl TypeScriptNode {
             TypeScriptPrimativeType::Number => type_string.push_str("number"),
             TypeScriptPrimativeType::Null => type_string.push_str("null"),
             TypeScriptPrimativeType::Object => {
-                let mut object_type_string = String::new();
-                object_type_string.push_str(&format!(
-                    "{{{}{}",
-                    &Self::newline_if_parent_not_array_node(parent_array_node),
-                    &Self::space_if_parent_not_root_node(node.root_node)
-                ));
-                for o in node.sub_items {
+                if type_output_cache.contains_key(&node.hash) {
+                    type_string.push_str(&type_output_cache[&node.hash].type_name);
+                } else {
+                    let mut object_type_string = String::new();
                     object_type_string.push_str(&format!(
-                        "{}{}{}",
-                        TypeScriptNode::to_type_string_helper(
-                            o,
-                            parent_array_node,
-                            indent_size + 1,
-                        ),
-                        &Self::space_if_parent_not_root_node(parent_array_node),
-                        &Self::semicolon_if_parent_array_node(parent_array_node)
+                        "{{{}{}",
+                        &Self::newline_if_parent_not_array_node(parent_array_node),
+                        &Self::space_if_parent_not_root_node(node.root_node)
                     ));
+                    for o in node.sub_items {
+                        object_type_string.push_str(&format!(
+                            "{}{}{}",
+                            TypeScriptNode::to_type_string_helper(
+                                o,
+                                parent_array_node,
+                                indent_size + 1,
+                                type_output_cache,
+                            ),
+                            &Self::space_if_parent_not_root_node(parent_array_node),
+                            &Self::semicolon_if_parent_array_node(parent_array_node)
+                        ));
+                    }
+                    object_type_string.push_str(&format!(
+                        "{}{}}}",
+                        &Self::space_if_parent_not_root_node(node.root_node),
+                        indent_string
+                    ));
+                    let object_type_output = object_type_string.clone();
+                    let lookup_table = node.lookup_table.lock().unwrap();
+                    if lookup_table.contains_key(&node.hash) && lookup_table[&node.hash] > 1 {
+                        let len = type_output_cache.len();
+                        let type_name = format!("DefaultType_{}", len);
+                        type_output_cache.insert(
+                            node.hash,
+                            TypeOutputCacheEntry::new(
+                                type_name.clone(),
+                                object_type_output.clone(),
+                            ),
+                        );
+                        type_string.push_str(&type_name);
+                    } else {
+                        type_string.push_str(&object_type_string.clone())
+                    }
                 }
-                object_type_string.push_str(&format!(
-                    "{}{}}}",
-                    &Self::space_if_parent_not_root_node(node.root_node),
-                    indent_string
-                ));
-                type_string.push_str(&object_type_string.clone())
             }
             TypeScriptPrimativeType::Array => {
                 let mut array_types_seen = HashSet::<String>::new();
                 for a in node.sub_items {
-                    let array_type =
-                        TypeScriptNode::to_type_string_helper(a, true, indent_size + 1);
+                    let array_type = TypeScriptNode::to_type_string_helper(
+                        a,
+                        true,
+                        indent_size + 1,
+                        type_output_cache,
+                    );
                     array_types_seen.insert(array_type);
                 }
                 if array_types_seen.len() == 0 {
@@ -188,17 +290,20 @@ impl TypeScriptNode {
 }
 
 fn walk_value_tree(v: &Value, key_name: Option<String>) -> Result<TypeScriptNode> {
-    walk_value_tree_helper(v, key_name, true)
+    let lookup_table = HashMap::<u64, usize>::new();
+    walk_value_tree_helper(v, key_name, true, Arc::new(Mutex::new(lookup_table)))
 }
 
 fn walk_value_tree_helper(
     v: &Value,
     key_name: Option<String>,
     root_node: bool,
+    lookup_table: Arc<Mutex<HashMap<u64, usize>>>,
 ) -> Result<TypeScriptNode> {
     match v {
         Value::String(_s) => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::String,
                 false,
                 false,
@@ -212,6 +317,7 @@ fn walk_value_tree_helper(
         }
         Value::Number(_n) => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::Number,
                 false,
                 false,
@@ -225,6 +331,7 @@ fn walk_value_tree_helper(
         }
         Value::Bool(_b) => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::Boolean,
                 false,
                 false,
@@ -238,6 +345,7 @@ fn walk_value_tree_helper(
         }
         Value::Null => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::Null,
                 false,
                 false,
@@ -251,6 +359,7 @@ fn walk_value_tree_helper(
         }
         Value::Array(a) => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::Array,
                 false,
                 false,
@@ -259,7 +368,12 @@ fn walk_value_tree_helper(
             );
             let mut sub_items = Vec::new();
             for v in a {
-                sub_items.push(walk_value_tree_helper(v, None, false)?);
+                sub_items.push(walk_value_tree_helper(
+                    v,
+                    None,
+                    false,
+                    lookup_table.clone(),
+                )?);
             }
             if let Some(name) = key_name {
                 node = node.with_name(name);
@@ -270,6 +384,7 @@ fn walk_value_tree_helper(
         }
         Value::Object(o) => {
             let mut node = TypeScriptNode::new(
+                lookup_table.clone(),
                 TypeScriptPrimativeType::Object,
                 false,
                 false,
@@ -282,6 +397,7 @@ fn walk_value_tree_helper(
                     v,
                     Option::Some(k.to_string()),
                     false,
+                    lookup_table.clone(),
                 )?);
             }
             if let Some(name) = key_name {
@@ -313,9 +429,19 @@ fn main() -> Result<()> {
 
     let v: Value = serde_json::from_str(input_file_content.as_str())
         .with_context(|| format!("could not parse json"))?;
-
-    let result = walk_value_tree(&v, None).unwrap();
+    let mut result: TypeScriptNode = walk_value_tree(&v, None).unwrap();
     let result_root_is_array = result.is_array.clone();
+    match args.squash_common_types {
+        Some(val) => {
+            if val {
+                result.calculate_hash();
+            }
+        }
+        None => {
+            result.calculate_hash();
+            ()
+        }
+    }
     let output_string = TypeScriptNode::to_type_string(result, result_root_is_array);
     if args.output_file.is_none() {
         event!(
@@ -342,7 +468,8 @@ mod tests {
     #[test]
     fn parses_string() {
         let val_tree = serde_json::from_str(r#""hello""#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = string;\n");
     }
@@ -359,7 +486,8 @@ mod tests {
           "#,
         )
         .unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(
             output_string,
@@ -370,7 +498,8 @@ mod tests {
     #[test]
     fn parses_number() {
         let val_tree = serde_json::from_str(r#"1"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = number;\n");
     }
@@ -378,7 +507,8 @@ mod tests {
     #[test]
     fn parses_bool() {
         let val_tree = serde_json::from_str(r#"true"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = boolean;\n");
     }
@@ -386,7 +516,8 @@ mod tests {
     #[test]
     fn parses_null() {
         let val_tree = serde_json::from_str(r#"null"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = null;\n");
     }
@@ -394,7 +525,8 @@ mod tests {
     #[test]
     fn parses_object() {
         let val_tree = serde_json::from_str(r#"{}"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = {\n};\n");
     }
@@ -402,7 +534,8 @@ mod tests {
     #[test]
     fn parses_array() {
         let val_tree = serde_json::from_str(r#"[]"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(output_string, "type DefaultType = any[];\n");
     }
@@ -410,18 +543,17 @@ mod tests {
     #[test]
     fn parses_object_with_array() {
         let val_tree = serde_json::from_str(r#"{ "test": [] }"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
-        assert_eq!(
-            output_string,
-            "type DefaultType = {\n  test: any[];\n };\n"
-        );
+        assert_eq!(output_string, "type DefaultType = {\n  test: any[];\n };\n");
     }
 
     #[test]
     fn parses_object_with_object() {
         let val_tree = serde_json::from_str(r#"{ "test": { "test": "test" } }"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(
             output_string,
@@ -434,22 +566,25 @@ mod tests {
         let val_tree =
             serde_json::from_str(r#"{ "test": [{ "test": "test" }, { "test": "test" }] }"#)
                 .unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
-        let output_string = TypeScriptNode::to_type_string(result, false);
+                let mut result = walk_value_tree(&val_tree, None).unwrap();
+                result.calculate_hash();
+                let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(
             output_string,
-            "type DefaultType = {\n  test: { test: string; }[];\n };\n"
+            "type DefaultType = {\n  test: DefaultType_0[];\n };\n\ntype DefaultType_0 = { test: string; }\n"
         );
     }
 
     #[test]
     fn parses_object_with_array_of_arrays() {
         let val_tree = serde_json::from_str(r#"{ "test": [[], []] }"#).unwrap();
-        let result = walk_value_tree(&val_tree, None).unwrap();
+        let mut result = walk_value_tree(&val_tree, None).unwrap();
+        result.calculate_hash();
         let output_string = TypeScriptNode::to_type_string(result, false);
         assert_eq!(
             output_string,
             "type DefaultType = {\n  test: any[][];\n };\n"
         );
     }
+
 }
